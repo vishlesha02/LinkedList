b1e6957b0c9e2b8b0f9e16a89306341e2168d4d6
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-05-08 20:21:25 +0530

    Add files via upload

--- LinkList.java
+++ LinkList.java
@@ -2,6 +2,7 @@ package Day10;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Collections;
 
 public class LinkList {
     public static void main(String[] args) {
@@ -11,17 +12,16 @@ public class LinkList {
         list.addFirst(56);
         list.add(30);
         list.add(70);
-        indx=list.indexOf(30);
-        list.add(indx,40);
-        indx=list.indexOf(40);
+        indx = list.indexOf(30);
+        list.add(indx, 40);
+        indx = list.indexOf(40);
         list.remove(indx);
         System.out.println("The Size of Linked List is : " + list.size());
 
         Iterator i = list.iterator();
-
+        Collections.sort(list);
         while (i.hasNext()) {
             System.out.println(i.next());
         }
     }
 }
-
