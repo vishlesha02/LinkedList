5bd40455ade1c5d60c983f70c3e19ea0c14e09fd
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-05-08 20:02:22 +0530

    Add files via upload

---
+++ LinkList.java
@@ -0,0 +1,16 @@
+package Day10;
+
+import java.util.LinkedList;
+
+public class LinkList {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList<Integer>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+
+        System.out.println(list);
+
+    }
+
+    }
